8eac270746b2eded459fffe4166afeb3ea1227ed
DELL-BRUNO\Bruno <bruno.panaget@yahoo.fr>
2017-10-29 21:55:14 +0100

    Initial commit

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,14 @@
+var nameVar = 'Andrew';
+var nameVar = 'Bruno';
+console.log('nameVar',nameVar);
+
+let nameLet = 'Andrew';
+nameLet = 'Bruno';
+//let nameLet = 'John';
+console.log('nameLet',nameLet);
+
+const nameConst = 'Andrew';
+//const nameConst = 'Bruno';
+//nameConst = 'Bruno';
+console.log('nameConst',nameConst);
+
