60b78bd9f0d7fec774aea29d867fdb92686de4b3
abcdevelop <bruno.panaget@yahoo.fr>
2017-10-31 14:32:43 +0100

    Setup webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,15 @@ module.exports = {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    module:{
-        rules:[{
-            loader:'babel-loader',
+    module: {
+        rules: [{
+            loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
         }]
     },
-    devtool:'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+    }
 };
